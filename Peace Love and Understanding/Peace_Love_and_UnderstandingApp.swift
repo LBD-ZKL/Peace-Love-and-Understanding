b076d3af7ee6250e0201c39f85e529d30969d2cc
Laura Martin Davis <lauraomartin@icloud.com>
2026-01-19 09:52:00 -0500

    Initial Commit

---
+++ Peace Love and Understanding/Peace_Love_and_UnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peace_Love_and_UnderstandingApp.swift
+//  Peace Love and Understanding
+//
+//  Created by Laura Becca Davis on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Peace_Love_and_UnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
